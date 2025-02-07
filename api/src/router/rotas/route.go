35d669d2912f9397507b5f4c63c95bcdb2d8efc9
Phyllipe Soares de Albuquerque <phyllipe.sjc@hotmail.com>
2025-02-07 15:40:31 -0300

    Adicionar routeUsers ao Router

--- api/src/router/rotas/route.go
+++ api/src/router/rotas/route.go
@@ -1,6 +1,10 @@
 package rotas
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 // Route represents a route in the application
 type Route struct {
@@ -8,4 +12,15 @@ type Route struct {
 	Method                string
 	Function              func(w http.ResponseWriter, r *http.Request)
 	RequestAuthentication bool
+}
+
+// Config receives all routes and returns a router with all routes configured
+func Config(r *mux.Router) *mux.Router {
+	routes := routeUsers
+
+	for _, route := range routes {
+		r.HandleFunc(route.URI, route.Function).Methods(route.Method)
+	}
+
+	return r
 }
