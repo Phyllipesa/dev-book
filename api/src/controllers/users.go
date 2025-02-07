41506903e7ffd168c993f89a75b85869056809e3
Phyllipe Soares de Albuquerque <phyllipe.sjc@hotmail.com>
2025-02-07 20:26:52 -0300

    Configurar função createUser controller

--- api/src/controllers/users.go
+++ api/src/controllers/users.go
@@ -1,10 +1,45 @@
 package controllers
 
-import "net/http"
+import (
+	"api/src/db"
+	"api/src/models"
+	"api/src/repository"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
 
 // CreateUser creates a user
 func CreateUser(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Create user"))
+	// request body
+	bodyRequest, erro := ioutil.ReadAll(r.Body)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	// unmarshal request body
+	var user models.User
+	if erro = json.Unmarshal(bodyRequest, &user); erro != nil {
+		log.Fatal(erro)
+	}
+
+	// create a db connection
+	db, erro := db.Connection()
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	// passing db connection to repository
+	repository := repository.NewRepositoryUsers(db)
+	userID, erro := repository.Create(user)
+
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	w.Write([]byte(fmt.Sprintf("User created with id %d", userID)))
 }
 
 // FindAllUsers finds all users
