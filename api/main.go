4d64262e8fedd326fbb6116fdca67b2cee626ae1
Phyllipe Soares de Albuquerque <phyllipe.sjc@hotmail.com>
2025-02-07 14:23:40 -0300

    Adicionar o router ao main

--- api/main.go
+++ api/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"api/src/router"
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Println("Application is running on port 5000")
+
+	r := router.Gerar()
+
+	log.Fatal(http.ListenAndServe(":5000", r))
 }
