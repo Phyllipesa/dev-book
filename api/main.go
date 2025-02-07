e5f9bacf6fe7614514e661dd3ce22f7ca4bae814
Phyllipe Soares de Albuquerque <phyllipe.sjc@hotmail.com>
2025-02-07 19:54:39 -0300

    Carregar as configurações do db no main

--- api/main.go
+++ api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"api/src/config"
 	"api/src/router"
 	"fmt"
 	"log"
@@ -8,8 +9,8 @@ import (
 )
 
 func main() {
+	config.ToLoad()
 	fmt.Println("Application is running on port 5000")
-
 	r := router.Generate()
 
 	log.Fatal(http.ListenAndServe(":5000", r))
