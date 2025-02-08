b25236209728497b777a16e1dd6d3b937cd17ef8
Phyllipe Soares de Albuquerque <phyllipe.sjc@hotmail.com>
2025-02-08 16:44:12 -0300

    Criar metodos de validação no struct user

--- api/src/models/User.go
+++ api/src/models/User.go
@@ -1,6 +1,10 @@
 package models
 
-import "time"
+import (
+	"errors"
+	"strings"
+	"time"
+)
 
 // User represents a user in the application
 type User struct {
@@ -10,4 +14,42 @@ type User struct {
 	Email     string    `json:"email,omitempty"`
 	Password  string    `json:"password,omitempty"`
 	CreatedAt time.Time `json:"createAt,omitempty"`
+}
+
+// Prepare prepares the user data
+func (user *User) Prepare() error {
+	if erro := user.validation(); erro != nil {
+		return erro
+	}
+
+	user.format()
+	return nil
+}
+
+// Validate validates the user data
+func (user *User) validation() error {
+	if user.Name == "" {
+		return errors.New("name is required")
+	}
+
+	if user.Nick == "" {
+		return errors.New("nick is required")
+	}
+
+	if user.Email == "" {
+		return errors.New("email is required")
+	}
+
+	if user.Password == "" {
+		return errors.New("password is required")
+	}
+
+	return nil
+}
+
+// format trims the user data
+func (user *User) format() {
+	user.Name = strings.TrimSpace(user.Name)
+	user.Nick = strings.TrimSpace(user.Nick)
+	user.Email = strings.TrimSpace(user.Email)
 }
