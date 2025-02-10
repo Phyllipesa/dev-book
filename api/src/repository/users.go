9d9fe6e9a81439560903f4781e12d928c61016a2
Phyllipe Soares de Albuquerque <phyllipe.sjc@hotmail.com>
2025-02-10 15:44:32 -0300

    Criar e configurar o método DeleteUser no respository

--- api/src/repository/users.go
+++ api/src/repository/users.go
@@ -117,3 +117,18 @@ func (repository users) UpdateUser(ID uint64, user models.User) error {
 
 	return nil
 }
+
+// Delete deletes a user from the database
+func (repository users) DeleteUser(ID uint64) error {
+	statement, erro := repository.db.Prepare("delete from users where id = ?")
+	if erro != nil {
+		return erro
+	}
+	defer statement.Close()
+
+	if _, erro = statement.Exec(ID); erro != nil {
+		return erro
+	}
+
+	return nil
+}
