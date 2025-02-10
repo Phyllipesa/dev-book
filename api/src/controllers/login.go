21c8a45c246c01747b36dac528f759af4dedf259
Phyllipe Soares de Albuquerque <phyllipe.sjc@hotmail.com>
2025-02-10 18:38:46 -0300

    Criar em configurar o controller para a rota login

---
+++ api/src/controllers/login.go
@@ -0,0 +1,48 @@
+package controllers
+
+import (
+	"api/src/db"
+	"api/src/models"
+	"api/src/repository"
+	"api/src/responses"
+	"api/src/security"
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+// Login is the controller for the login route
+func Login(w http.ResponseWriter, r *http.Request) {
+	requestBody, erro := io.ReadAll(r.Body)
+	if erro != nil {
+		responses.Error(w, http.StatusUnprocessableEntity, erro)
+		return
+	}
+
+	var user models.User
+	if erro = json.Unmarshal(requestBody, &user); erro != nil {
+		responses.Error(w, http.StatusBadRequest, erro)
+		return
+	}
+
+	db, erro := db.Connection()
+	if erro != nil {
+		responses.Error(w, http.StatusInternalServerError, erro)
+		return
+	}
+	defer db.Close()
+
+	repository := repository.NewRepositoryUsers(db)
+	userFromDB, erro := repository.FindByEmail(user.Email)
+	if erro != nil {
+		responses.Error(w, http.StatusInternalServerError, erro)
+		return
+	}
+
+	if erro = security.VerifyPassword(userFromDB.Password, user.Password); erro != nil {
+		responses.Error(w, http.StatusUnauthorized, erro)
+		return
+	}
+
+	w.Write([]byte("Logged in"))
+}
