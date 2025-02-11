c867c31e4cba5b64383541358f9eb7a270b5cce6
Phyllipe Soares de Albuquerque <phyllipe.sjc@hotmail.com>
2025-02-11 14:18:28 -0300

    Criar função para extração de token

--- api/src/authentication/token.go
+++ api/src/authentication/token.go
@@ -2,6 +2,8 @@ package authentication
 
 import (
 	"api/src/config"
+	"net/http"
+	"strings"
 	"time"
 
 	jwt "github.com/dgrijalva/jwt-go"
@@ -15,4 +17,15 @@ func CreateToken(userID uint64) (string, error) {
 	permissions["userID"] = userID
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, permissions)
 	return token.SignedString([]byte(config.SecretKey)) // secret
+}
+
+// extractToken if token in correct format, extract from request and return
+func extractToken(r *http.Request) string {
+	token := r.Header.Get("Authorization")
+
+	if len(strings.Split(token, " ")) == 2 {
+		return strings.Split(token, " ")[1]
+	}
+
+	return ""
 }
