d68fa62eef8c2e4db51202a9ec0ef2d8ecf32562
Phyllipe Soares de Albuquerque <phyllipe.sjc@hotmail.com>
2025-02-11 14:20:33 -0300

    Adicionar a validação do token ao middleware de autenticação

--- api/src/middlewares/middlewares.go
+++ api/src/middlewares/middlewares.go
@@ -1,7 +1,8 @@
 package middlewares
 
 import (
-	"fmt"
+	"api/src/authentication"
+	"api/src/responses"
 	"log"
 	"net/http"
 )
@@ -17,7 +18,10 @@ func Logger(next http.HandlerFunc) http.HandlerFunc {
 // Authentication verify if user is authenticated
 func Authentication(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("Authentication")
+		if erro := authentication.ValidationToken(r); erro != nil {
+			responses.Error(w, http.StatusUnauthorized, erro)
+			return
+		}
 		next(w, r)
 	}
 }
