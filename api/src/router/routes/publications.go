e8a6d302e8d2e502c68dc01c21365213604a186a
Phyllipe Soares de Albuquerque <phyllipe.sjc@hotmail.com>
2025-02-12 16:28:38 -0300

    Criar a rota publications

---
+++ api/src/router/routes/publications.go
@@ -0,0 +1,39 @@
+package routes
+
+import (
+	"api/src/controllers"
+	"net/http"
+)
+
+var routePublications = []Route{
+	{
+		URI:                   "/publications",
+		Method:                http.MethodPost,
+		Function:              controllers.CreatePublication,
+		RequestAuthentication: true,
+	},
+	{
+		URI:                   "/publications",
+		Method:                http.MethodGet,
+		Function:              controllers.FindPublication,
+		RequestAuthentication: true,
+	},
+	{
+		URI:                   "/publications/{publicationId}",
+		Method:                http.MethodGet,
+		Function:              controllers.FindPublicationById,
+		RequestAuthentication: true,
+	},
+	{
+		URI:                   "/publications/{publicationId}",
+		Method:                http.MethodPut,
+		Function:              controllers.UpdatePublication,
+		RequestAuthentication: true,
+	},
+	{
+		URI:                   "/publications/{publicationId}",
+		Method:                http.MethodDelete,
+		Function:              controllers.DeletePublication,
+		RequestAuthentication: true,
+	},
+}
