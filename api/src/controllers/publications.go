e1cff97bd19f0c52584d29efe15ffe824251a490
Phyllipe Soares de Albuquerque <phyllipe.sjc@hotmail.com>
2025-02-12 18:06:30 -0300

    Configurar o método CreatePublication

--- api/src/controllers/publications.go
+++ api/src/controllers/publications.go
@@ -1,10 +1,58 @@
 package controllers
 
-import "net/http"
+import (
+	"api/src/authentication"
+	"api/src/db"
+	"api/src/models"
+	"api/src/repository"
+	"api/src/responses"
+	"encoding/json"
+	"io"
+	"net/http"
+)
 
 // CreatePublication add a publication in database
 func CreatePublication(w http.ResponseWriter, r *http.Request) {
+	userIdFromToken, erro := authentication.ExtractUserID(r)
+	if erro != nil {
+		responses.Error(w, http.StatusUnauthorized, erro)
+		return
+	}
 
+	bodyRequest, erro := io.ReadAll(r.Body)
+	if erro != nil {
+		responses.Error(w, http.StatusUnprocessableEntity, erro)
+		return
+	}
+
+	var publication models.Publication
+	if erro = json.Unmarshal(bodyRequest, &publication); erro != nil {
+		responses.Error(w, http.StatusBadRequest, erro)
+		return
+	}
+
+	publication.AuthorID = userIdFromToken
+
+	if erro = publication.Prepare(); erro != nil {
+		responses.Error(w, http.StatusBadRequest, erro)
+		return
+	}
+
+	db, erro := db.Connection()
+	if erro != nil {
+		responses.Error(w, http.StatusInternalServerError, erro)
+		return
+	}
+	defer db.Close()
+
+	repository := repository.NewRepositoryPublications(db)
+	publication.ID, erro = repository.Create(publication)
+	if erro != nil {
+		responses.Error(w, http.StatusInternalServerError, erro)
+		return
+	}
+
+	responses.JSON(w, http.StatusCreated, publication)
 }
 
 // FindPublication find publications that would appear in the user's feed
