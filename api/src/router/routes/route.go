9e9c278e245063271b6f94c084ab52b95ae4b3cd
Phyllipe Soares de Albuquerque <phyllipe.sjc@hotmail.com>
2025-02-12 16:29:48 -0300

    Adicionar a rota de publications ao Router

--- api/src/router/routes/route.go
+++ api/src/router/routes/route.go
@@ -19,6 +19,7 @@ type Route struct {
 func Config(r *mux.Router) *mux.Router {
 	routes := routeUsers
 	routes = append(routes, routeLogin)
+	routes = append(routes, routePublications...)
 
 	for _, route := range routes {
 		if route.RequestAuthentication {
