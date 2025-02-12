8d6759d80d68d8a99b2bdb6baf7848cfab4f73dd
Phyllipe Soares de Albuquerque <phyllipe.sjc@hotmail.com>
2025-02-12 18:07:11 -0300

    Criar e configurar o método Create no repository

--- api/src/repository/publications.go
+++ api/src/repository/publications.go
@@ -1,12 +1,40 @@
 package repository
 
-import "database/sql"
+import (
+	"api/src/models"
+	"database/sql"
+)
 
 type publications struct {
 	db *sql.DB
 }
 
 // NewRepositoryPublications creates a new repository publications instance
-func NewRepositoryPublications(db *sql.DB) *users {
-	return &users{db}
+func NewRepositoryPublications(db *sql.DB) *publications {
+	return &publications{db}
+}
+
+// Create inserts a publication in database
+func (repository publications) Create(publication models.Publication) (uint64, error) {
+	statement, erro := repository.db.Prepare(
+		`insert into publications (title, content, author_id) values (?, ?, ?)`,
+	)
+	if erro != nil {
+		return 0, erro
+	}
+	defer statement.Close()
+
+	result, erro := statement.Exec(
+		publication.Title, publication.Content, publication.AuthorID,
+	)
+	if erro != nil {
+		return 0, erro
+	}
+
+	lastIdInserted, erro := result.LastInsertId()
+	if erro != nil {
+		return 0, erro
+	}
+
+	return uint64(lastIdInserted), nil
 }
