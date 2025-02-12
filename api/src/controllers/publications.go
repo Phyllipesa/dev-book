52a0159bddcdbf804c0d9d86557981a53f44b4d5
Phyllipe Soares de Albuquerque <phyllipe.sjc@hotmail.com>
2025-02-12 16:30:38 -0300

    Criar controller publications

---
+++ api/src/controllers/publications.go
@@ -0,0 +1,28 @@
+package controllers
+
+import "net/http"
+
+// CreatePublication add a publication in database
+func CreatePublication(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// FindPublication find publications that would appear in the user's feed
+func FindPublication(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// FindPublicationById find a publication
+func FindPublicationById(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// UpdatePublication update a publication
+func UpdatePublication(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// DeletePublication delete a publication
+func DeletePublication(w http.ResponseWriter, r *http.Request) {
+
+}
