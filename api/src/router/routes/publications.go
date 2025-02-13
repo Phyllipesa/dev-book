f32b86f3aa15150a883c7795afc51a2e13d3bc84
Phyllipe Soares de Albuquerque <phyllipe.sjc@hotmail.com>
2025-02-12 21:46:40 -0300

    Criar rota para curtir uma publicação

--- api/src/router/routes/publications.go
+++ api/src/router/routes/publications.go
@@ -42,4 +42,10 @@ var routePublications = []Route{
 		Function:              controllers.FindPublicationsByUser,
 		RequestAuthentication: true,
 	},
+	{
+		URI:                   "/publications/{publicationId}/like",
+		Method:                http.MethodPost,
+		Function:              controllers.LikePublication,
+		RequestAuthentication: true,
+	},
 }
