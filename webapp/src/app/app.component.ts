8c6532997bf39dd7b503a3b0387aba04884ebd6f
Phyllipe Soares de Albuquerque <phyllipe.sjc@hotmail.com>
2025-02-18 14:16:53 -0300

    Importar o angularMaterialModule no appComponent

--- webapp/src/app/app.component.ts
+++ webapp/src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AngularMaterialModule } from './angular-material/angular-material.module';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AngularMaterialModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'webapp';
-}
+export class AppComponent {}
