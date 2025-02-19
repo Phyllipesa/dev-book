bad48dd2c374f37115dcd09c09348eda4451dd9e
Phyllipe Soares de Albuquerque <phyllipe.sjc@hotmail.com>
2025-02-18 21:40:53 -0300

    Adicionar provideHttpClient ao appConfig

--- webapp/src/app/app.config.ts
+++ webapp/src/app/app.config.ts
@@ -1,7 +1,8 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
@@ -9,5 +10,6 @@ export const appConfig: ApplicationConfig = {
         provideZoneChangeDetection({ eventCoalescing: true }),
         provideRouter(routes),
         provideAnimationsAsync(),
+        provideHttpClient(),
     ],
 };
