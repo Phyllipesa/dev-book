0c3451c36fb0a79aeb0886fbc727d14103b9c3fb
Phyllipe Soares de Albuquerque <phyllipe.sjc@hotmail.com>
2025-02-20 13:39:01 -0300

    Criar e configurar login em loginService

--- webapp/src/app/services/login.service.ts
+++ webapp/src/app/services/login.service.ts
@@ -1,8 +1,21 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import type { Observable } from 'rxjs';
+import type { ILoginResponse } from '../interfaces/responses/login-response-interface';
 
 @Injectable({
     providedIn: 'root',
 })
 export class LoginService {
-    constructor() {}
+    private readonly _httpClient = inject(HttpClient);
+
+    login(email: string, password: string): Observable<ILoginResponse> {
+        return this._httpClient.post<ILoginResponse>(
+            'http://localhost:5000/login',
+            {
+                email,
+                password,
+            }
+        );
+    }
 }
